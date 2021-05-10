03c935417744f0b41dd2c7ad65cb5bd36e1a139c
RAM-VATS <75200197+RAM-VATS@users.noreply.github.com>
2021-05-10 19:16:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+//const Engine = Matter.Engine;
+//const World= Matter.World;
+//const Bodies = Matter.Bodies;
+//const Constraint = Matter.Constraint;
+
+//var engine, world;
+let snow = [];
+//let gravity;
+
+function setup() {
+  createCanvas(1000,1200);
+  //engine = Engine.create();
+
+//gravity = createVector(0,0.03);
+
+//santa1 = new santa(windowWidth/2,windowHeight/8);
+}
+
+function draw() {
+  background(0);
+  //Engine.update(engine);
+
+  snow.push(new snowflake())
+
+//santa1.display()
+for(flake of snow){
+  //flake.applyForce(gravity);
+  flake.update();
+  flake.render();
+}
+
+//for(let i = snow.length-1; i >=0; i--){
+  //if(snow[i].offScreen()){
+    //snow.splice(i,1);
+  //}
+//}
+//textSize(35)
+//fill("white");
+//print("SCROLL DOWN TO SEE THE FLAKES",400,200)
+}
---
+++ snowflake.js
@@ -0,0 +1,28 @@
+class snowflake{
+    constructor(){
+        let x = random(width);
+       let y = random(0,-10);
+        this.pos = createVector(x,y);
+        this.vel = createVector(random(-2,2),2);
+        //this.vel = createVector(0)
+        this.acc = createVector();
+      //  this.r = getRandomSize();
+        //this.terminalV = this.r;
+
+        //this.Image = loadImage("pics/snow.jpg");
+    }
+
+    update(){
+        
+        //this.acc = gravity;
+        //this.vel.add(this.acc);
+        this.pos.add(this.vel);
+
+    }
+
+render(){
+    stroke(255);
+    strokeWeight(4);
+    point(this.pos.x,this.pos.y)
+}
+}
